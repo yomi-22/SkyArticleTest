1ad677f149dbf0cbc951368e7adf4f965d541cb1
Ladokun Yomi <y.ladokun@reply.com>
2021-05-16 21:40:22 +0100

    Add project files.

---
+++ SkyArticle/ApiHelper/SkyCommonStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SkyArticle.ApiHelper
+{
+    public class SkyCommonStep
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public  static void Initialize()
+
+        {
+            ApiClient = new HttpClient();
+          //  ApiClient.BaseAddress = new Uri("https://5f99522350d84900163b8737.mockapi.io/tech-test/articles");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+        }
+    }
+}
---
+++ SkyArticle/Steps/ArticleSteps.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SkyArticle.Steps
+{
+    public class ArticleSteps
+    {
+        string url = "";
+        [HttpGet]
+        public async Task LoadArticle(int articleUrl)
+        {
+
+
+            if (articleUrl > 0)
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/{articleUrl}";
+            }
+            else
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/";
+            }
+
+            using HttpResponseMessage response = await ApiHelper.SkyCommonStep.ApiClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                response.StatusCode.Equals(200);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        [HttpPost]
+        public async Task InvalidArticleRequest(int articleUrl)
+        {
+
+            if (articleUrl > 0)
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/{articleUrl}";
+            }
+            else
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/";
+            }
+
+            using HttpResponseMessage response = await ApiHelper.SkyCommonStep.ApiClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                response.StatusCode.Equals(404);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        [HttpPut]
+        public async Task InvalidPutArticleRequest(int articleUrl)
+        {
+
+            if (articleUrl > 0)
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/{articleUrl}";
+            }
+            else
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/";
+            }
+
+            using HttpResponseMessage response = await ApiHelper.SkyCommonStep.ApiClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                response.StatusCode.Equals(404);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        [HttpDelete]
+        public async Task InvalidDeleteArticleRequest(int articleUrl)
+        {
+
+            if (articleUrl > 0)
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/{articleUrl}";
+            }
+            else
+            {
+                url = $"https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/";
+            }
+
+            using HttpResponseMessage response = await ApiHelper.SkyCommonStep.ApiClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                response.StatusCode.Equals(404);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
+
+
